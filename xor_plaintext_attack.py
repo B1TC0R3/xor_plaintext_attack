c921bf312d42e9f504d1a237054558a31e2485a4
Thomas Gingele <thomas@gingele.de>
2023-10-09 23:12:54 +0200

    Add script

---
+++ xor_plaintext_attack.py
@@ -0,0 +1,46 @@
+# Copyright © 2023 Thomas Gingele https://github.com/B1TC0R3
+import argparse
+import os
+
+
+def get_args() -> argparse.Namespace:
+    parser = argparse.ArgumentParser(
+        prog='XOR Plaintext Cracker',
+        description='This script will attempt to find the secret key of a XOR encoded string by performing a plaintext attack',
+        epilog='Copyright © 2023 Thomas Gingele https://github.com/B1TC0R3'
+    )
+
+    parser.add_argument(
+        '-c',
+        '--cipher',
+        help='The XOR encoded string',
+        required=True
+    )
+
+    parser.add_argument(
+        '-p',
+        '--plaintext',
+        help='The plaintext string',
+        required=True
+    )
+
+    return parser.parse_args()
+
+
+def xor(cipher, key) -> bytearray:
+    return bytearray(
+        a ^ b for a, b in zip(*map(bytearray, [cipher, key]))
+    )
+
+
+def main():
+    args      = get_args()
+    cipher    = args.cipher.encode('utf-8')
+    plaintext = args.plaintext.encode('utf-8')
+    key       = str(xor(cipher, plaintext), 'utf-8')
+
+    print(key)
+
+
+if __name__ == '__main__':
+    main()
